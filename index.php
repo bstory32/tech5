c60dbeb26d78bf28e2abf592c716869bb438defc
Neil Sammons <sneil@Neils-MacBook-Pro.local>
2014-08-26 22:59:12 -0500

    modified newest version of homepage

--- index.php
+++ index.php
@@ -12,9 +12,10 @@
   <title>Tech 5 Designs</title>
 
   <!-- Bootstrap core CSS -->
-
+  <link href='http://fonts.googleapis.com/css?family=Rufina:400,700' rel='stylesheet' type='text/css'>
   <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,300' rel='stylesheet' type='text/css'>
   <link href='http://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>
+  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
   <link href="public/css/bootstrap.min.css" rel="stylesheet">
   <link href="public/css/main.css" rel="stylesheet">
 
@@ -45,88 +46,125 @@
           <div class="row">
             <div class="col-sm-12"> 
               <div class="message-details">
+                <img src="public/images/tech5_designs_dark.png" alt="">
                 <h1>We build websites<br>worth sharing</h1>
                 <span>Established in Chicago in 2013</span>
                 <hr>
                 <a href="#" class="btn btn-primary">View Our Work</a>
-
               </div>
             </div>
           </div>
         </div>
       </div>  
 
-
-      <!-- <div class="content-message">
+      <div class="portfolio-section">
        <div class="container">
          <div class="row">
            <div class="col-sm-12">
-            <h2>Content Focused, Results Driven</h2>
+            <h2>We're more than developers <br> we're partners</h2>
+            <hr>
+          </div>
+        </div>
+        <div class="projects">
+          <div class="row">
+            <div class="col-sm-3">
+              <h3>Muddrunner</h3>
+              <img src="public/images/muddrunner-image.jpg">
+            </div>
+            <div class="col-sm-3">
+              <h3>Commercial Connected</h3>
+              <img src="public/images/commercial-connected-image.jpg">
+            </div>
+            <div class="col-sm-3">
+              <h3>Project</h3>
+              <img src="http://placehold.it/200x150">
+            </div>
+            <div class="col-sm-3">
+              <h3>Project</h3>
+              <img class="hexagon" src="http://placehold.it/200x150">
+            </div>
+          </div>
+        </div>
+        <div class="team">
+          <div class="row">
+            <div class="col-sm-12">
+              <a href="#" class="btn btn-primary">Team Behind the Designs</a>
+            </div>
           </div>
         </div>
       </div>
     </div>
 
-    <div class="content-details">
+    <div class="team-section">
       <div class="container">
         <div class="row">
           <div class="col-sm-12">
-            <p>
-              We build beautiful websites that drive results, with a touch creative vision.  Take a look at what we offer.
-            </p>
+            <h2>The Dream Team</h2>
             <hr>
           </div>
         </div>
-        <div class="projects-section">
+        <div class="team-members">
           <div class="row">
-            <div class="col-sm-4">
-              <h3>Project</h3>
-              <img src="http://placehold.it/350x150">
+            <div class="col-sm-3">
+              <h3>Brian Story</h3>
+              <img src="http://placehold.it/200x150">
             </div>
-            <div class="col-sm-4">
-              <h3>Project</h3>
-              <img src="http://placehold.it/350x150">
+            <div class="col-sm-3">
+              <h3>Neil Sammons</h3>
+              <img src="http://placehold.it/200x150">
             </div>
-            <div class="col-sm-4">
-              <h3>Project</h3>
-              <img src="http://placehold.it/350x150">
+            <div class="col-sm-3">
+              <h3>Rafal Something</h3>
+              <img src="http://placehold.it/200x150">
+            </div>
+            <div class="col-sm-3">
+              <h3>Our Animals</h3>
+              <img src="http://placehold.it/200x150">
             </div>
           </div>
         </div>
-        <div class="projects-section">
+        <div class="contact">
           <div class="row">
-            <div class="col-sm-4">
-              <h3>Project</h3>
-              <img src="http://placehold.it/350x150">
-            </div>
-            <div class="col-sm-4">
-              <h3>Project</h3>
-              <img src="http://placehold.it/350x150">
-            </div>
-            <div class="col-sm-4">
-              <h3>Project</h3>
-              <img src="http://placehold.it/350x150">
-            </div>
-          </div>
-        </div>
-        <div class="projects-section">
-          <div class="row">
-            <div class="col-sm-4">
-              <h3>Project</h3>
-              <img src="http://placehold.it/350x150">
-            </div>
-            <div class="col-sm-4">
-              <h3>Project</h3>
-              <img src="http://placehold.it/350x150">
-            </div>
-            <div class="col-sm-4">
-              <h3>Project</h3>
-              <img src="http://placehold.it/350x150">
+            <div class="col-sm-12">
+              <p>Any Questions?</p>
+              <a href="#" class="btn btn-primary">Contact Us</a>
             </div>
           </div>
         </div>
       </div>
-    </div> -->
+    </div>
+
+    <div class="contact-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <h2>Let's Get the conversation started!</h2>
+            <hr>
+          </div>
+        </div>
+        <div class="contact-details">
+          <div class="row">
+            <div class="col-sm-4">
+              <h3>Talk</h3>
+              <i class="fa fa-phone"></i>
+              <p>847.544.6978</p>
+            </div>
+            <div class="col-sm-4">
+              <h3>Walk</h3>
+              <i class="fa fa-male"></i>
+              <p>7839 W Summerdale Avenue, Chicago, IL 60656</p>
+            </div>
+            <div class="col-sm-4">
+              <h3>Follow</h3>
+              <i class="fa fa-facebook"></i>
+              <p>Facebook: Tech 5 Designs</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
 
 
 
