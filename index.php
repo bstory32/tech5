8289247ef2b31fc8198942a1141cc547439f2b26
Neil Sammons <sneil@Neils-MacBook-Pro.local>
2014-10-02 14:15:19 -0500

    final tweaks before deployment

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
   <link href='http://fonts.googleapis.com/css?family=Rufina:400,700' rel='stylesheet' type='text/css'>
   <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,300' rel='stylesheet' type='text/css'>
   <link href='http://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Lato:300,400' rel='stylesheet' type='text/css'>
   <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
   <link href="public/css/bootstrap.min.css" rel="stylesheet">
   <link href="public/css/main.css" rel="stylesheet">
@@ -28,36 +29,44 @@
     </head>
 
     <body>
-      <nav>
-        <ul>
-          <li><a href="#about">Portfolio</a></li>
-          <li><a href="#contact">Services</a></li>
-          <li>
-            <a href="#"><img src="public/images/tech5-logo-5.png"></a>
-          </li>
-          <li><a href="#contact">Leadership</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
+      <nav role="navigation">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#"><img src="public/images/tech5_light.png" alt=""></a>
+        <div class="collapse navbar-collapse">
+          <ul class="main-nav">
+            <li><a href="#portfolio-section">Portfolio</a></li>
+            <li><a href="#services-section">Web Design</a></li>
+            <li>
+              <a href="#main-message-section"><img src="public/images/tech5-logo-5.png"></a>
+            </li>
+            <li><a href="#team-section">Leadership</a></li>
+            <li><a href="#contact-section">Contact</a></li>
+          </ul>
+        </div>
       </nav>
 
-
-      <div class="main-message">
+      <div class="main-message" id="main-message-section">
         <div class="container">
           <div class="row">
             <div class="col-sm-12"> 
               <div class="message-details">
                 <img src="public/images/tech5_designs_dark.png" alt="">
+                <span>Established in Chicago</span>
                 <h1>We build websites<br>worth sharing</h1>
-                <span>Established in Chicago in 2013</span>
                 <hr>
-                <a href="#" class="btn btn-primary">View Our Work</a>
+                <a href="#portfolio-section" class="btn btn-primary">View Our Latest Work</a>
               </div>
             </div>
           </div>
         </div>
       </div>  
 
-      <div class="portfolio-section">
+      <div class="portfolio-section" id="portfolio-section">
        <div class="container">
          <div class="row">
            <div class="col-sm-12">
@@ -67,102 +76,184 @@
         </div>
         <div class="projects">
           <div class="row">
-            <div class="col-sm-3">
+            <div class="col-sm-6">
               <h3>Muddrunner</h3>
-              <img src="public/images/muddrunner-image.jpg">
+              <a href="#"><img src="public/images/muddrunner-screen-shot.jpg"></a>
             </div>
-            <div class="col-sm-3">
+            <div class="col-sm-6">
               <h3>Commercial Connected</h3>
-              <img src="public/images/commercial-connected-image.jpg">
+              <a href="#"><img src="public/images/commercialconnected-screen-shot.jpg"></a>
             </div>
-            <div class="col-sm-3">
+            <!-- <div class="col-sm-3">
               <h3>Project</h3>
               <img src="http://placehold.it/200x150">
             </div>
             <div class="col-sm-3">
               <h3>Project</h3>
               <img class="hexagon" src="http://placehold.it/200x150">
-            </div>
+            </div> -->
           </div>
         </div>
         <div class="team">
           <div class="row">
             <div class="col-sm-12">
-              <a href="#" class="btn btn-primary">Team Behind the Designs</a>
+              <a href="#team-section" class="btn btn-primary">Team Behind the Designs</a>
             </div>
           </div>
         </div>
       </div>
     </div>
 
-    <div class="team-section">
+    <div class="services-section" id="services-section">
       <div class="container">
         <div class="row">
           <div class="col-sm-12">
-            <h2>The Dream Team</h2>
+            <h2>Content focused <br> results driven</h2>
+            <hr>
+          </div>
+          <div class="row services-details">
+            <div class="col-sm-4 marketing">
+              <h3>Marketing Sites</h3>
+              <i class="fa fa-desktop"></i>
+              <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis laborum ex perspiciatis ab. Voluptatum fugiat iure accusamus dolorem laborum doloribus corrupti dicta molestiae quasi unde aliquam vitae, adipisci nam nisi.</p> -->
+            </div>
+            <div class="col-sm-4 ecommerce">
+              <h3>e-Commerce Sites</h3> 
+              <i class="fa fa-shopping-cart"></i>
+              <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis laborum ex perspiciatis ab. Voluptatum fugiat iure accusamus dolorem laborum doloribus corrupti dicta molestiae quasi unde aliquam vitae, adipisci nam nisi.</p> -->
+            </div>
+            <div class="col-sm-4 custom">
+              <h3>Custom Solutions</h3>
+              <i class="fa fa-line-chart"></i>
+              <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis laborum ex perspiciatis ab. Voluptatum fugiat iure accusamus dolorem laborum doloribus corrupti dicta molestiae quasi unde aliquam vitae, adipisci nam nisi.</p> -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="team-section" id="team-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <h2>Our Leadership Team</h2>
             <hr>
           </div>
         </div>
         <div class="team-members">
           <div class="row">
-            <div class="col-sm-3">
+            <div class="col-sm-6">
               <h3>Brian Story</h3>
-              <img src="http://placehold.it/200x150">
+              <img src="public/images/brian-story-profile.jpg">
             </div>
-            <div class="col-sm-3">
+            <div class="col-sm-6">
               <h3>Neil Sammons</h3>
-              <img src="http://placehold.it/200x150">
+              <img src="public/images/neil-sammons-profile.jpg">
             </div>
-            <div class="col-sm-3">
+            <!-- <div class="col-sm-3">
               <h3>Rafal Something</h3>
               <img src="http://placehold.it/200x150">
             </div>
             <div class="col-sm-3">
               <h3>Our Animals</h3>
               <img src="http://placehold.it/200x150">
-            </div>
+            </div> -->
           </div>
         </div>
         <div class="contact">
           <div class="row">
             <div class="col-sm-12">
-              <p>Any Questions?</p>
-              <a href="#" class="btn btn-primary">Contact Us</a>
+              <!-- <p>Any Questions?</p> -->
+              <a href="#contact-section" class="btn btn-primary">Tell Us About Your Project</a>
             </div>
           </div>
         </div>
       </div>
     </div>
 
-    <div class="contact-section">
+    <div class="contact-section" id="contact-section">
       <div class="container">
         <div class="row">
-          <div class="col-sm-12">
+          <div class="col-sm-12 contact-message">
             <h2>Let's Get the conversation started!</h2>
             <hr>
           </div>
         </div>
-        <div class="contact-details">
-          <div class="row">
-            <div class="col-sm-4">
-              <h3>Talk</h3>
-              <i class="fa fa-phone"></i>
-              <p>847.544.6978</p>
+        <div class="col-sm-12 contact-details">
+          <form role="form" action="sendmail.php" method="POST">
+            <div class="form-group">
+              <label for="contactname">Contact Name*</label>
+              <input type="text" name="formName" class="form-control" id="contactname" placeholder="Name" required>
             </div>
-            <div class="col-sm-4">
-              <h3>Walk</h3>
-              <i class="fa fa-male"></i>
-              <p>7839 W Summerdale Avenue, Chicago, IL 60656</p>
+            <div class="form-group">
+              <label for="company">Company</label>
+              <input type="text" name="formCompany" class="form-control" id="contactemail" placeholder="Company">
             </div>
-            <div class="col-sm-4">
-              <h3>Follow</h3>
-              <i class="fa fa-facebook"></i>
-              <p>Facebook: Tech 5 Designs</p>
+            <div class="form-group">
+              <label for="contactphonenumber">Phone Number*</label>
+              <input type="tel" name="formTel" class="form-control" id="contactphonenumber" placeholder="Number" required>
             </div>
-          </div>
+            <div class="form-group">
+              <label for="contactemail">Email*</label>
+              <input type="email" name="formEmail" class="form-control" id="contactphonenumber" placeholder="Email" required>
+            </div>
+            <label for="contact-budget">Budget</label>
+            <select class="form-control">
+              <option value="1">$1000-$5000</option>
+              <option value="2">$5000-$10,000</option>
+              <option value="3">$10,000-$20,000</option>
+              <option value="4">$20,000 +</option>
+              <option value="5">Requires Estimation</option>
+            </select>
+            <div class="form-group">
+              <label for="contactmessage">Message</label>
+              <textarea class="form-control" name="formMessage" rows="3" id="contactmessage" placeholder="Message"></textarea>
+            </div>
+            <input type="submit" class="btn btn-primary" value="Submit"></input>
+          </form>
         </div>
       </div>
     </div>
+    
+    <footer>
+      <div class="created-by">
+        <div class="container">
+          <div class="row footer-content">
+            <div class="col-sm-3 footer-logo">
+              <a href="#main-message-section"><img src="public/images/tech5_light.png" alt=""></a>
+            </div>
+            <div class="col-sm-7 nav-links">
+              <ul>
+                <li>
+                  <a href="#portfolio-section">Portfolio</a>
+                </li>
+                <li>
+                  <a href="#services-section">Web Design</a>
+                </li>
+                <li>
+                  <a href="#team-section">Leadership</a>
+                </li>
+                <li>
+                  <a href="#contact-section">Contact</a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-sm-2 social-links">
+              <!-- <ul class="">
+                <li>
+                  <a href="#"><i class="fa fa-facebook-square"></i></a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa fa-twitter-square"></i></a>
+                </li>
+              </ul> -->
+            </div>
+          </div>
+        </div>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+      </div>
+    </footer>
+
 
 
 
